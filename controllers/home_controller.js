8fe3f527d8e2c4ee375f771eb49491da97316125
Mohd Faizan <gitfaizan0496@gmail.com>
2023-04-21 01:52:10 +0530

    adding partials and layouts in the projct

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,9 @@
 module.exports.home=function(req,res){
-return res.end('<h1>Express is Up for codeial!!</h1>')
+// return res.end('<h1>Express is Up for codeial!!</h1>')
+return res.render('home',{
+    title:"home",
+
+})
 
 }
 
--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const express=require('express');
 const app=express();
 const port=8000;
 
+//setting up the layouts
+
+const expressLayouts=require('express-ejs-layouts');
+
+app.use(express.static('./assets'));
+
+
+app.use(expressLayouts);
+
+// extract styles and scripts from subPages into the Layouts
+
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+
+
 // setting up the view engine
 
 app.set('view engine','ejs');

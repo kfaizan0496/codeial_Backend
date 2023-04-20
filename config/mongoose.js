05a92746a516528eca67c63a6037aa5751bf35fa
Mohd Faizan <gitfaizan0496@gmail.com>
2023-04-21 02:31:10 +0530

    connecting to the db

---
+++ config/mongoose.js
@@ -0,0 +1,27 @@
+// const mongoose=require('mongoose');
+// mongoose.connect('mongodb://localhost/codeial_development');
+
+// const db =mongoose.Connection;
+// db.on('error',console.error.bind(console,"Error Connecting to MongoDB"));
+
+// db.once('open',function(){
+//     console.log("Connecting to ::MongoDB");
+// })
+
+
+// module.exports=db;
+
+// above code is not working....
+
+
+
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
+const mongoDB = "mongodb://127.0.0.1/codeial_development"; 
+main().catch(err => console.log(err));
+async function main() {
+  await   mongoose.connect(mongoDB);
+  console.log(' db is connected');
+}
+
+// module.exports=db;

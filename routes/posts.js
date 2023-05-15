ddcab055313b1807b824e5eedd8829bf8c7f3a70
Mohd Faizan <gitfaizan0496@gmail.com>
2023-05-15 12:14:33 +0530

    check authentication is used in create post and no one user create  post untill unless sign in

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,10 @@
 const express =require('express');
 const router=express.Router();
+const passport=require('../config/passport-local-strategy');
 
 const postController=require('../controllers/post_controller');
 
-router.post('/create',postController.create);
+router.post('/create',passport.checkAuthentication,postController.create);
 
 
 module.exports=router;
